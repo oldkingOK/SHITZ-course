0672e47eafd748dea9f655ccb38669f9d08bd9f1
oldkingOK <oldkingok@qq.com>
2024-12-26 20:26:18 +0800

    getcookie

--- getcookie.py
+++ getcookie.py
@@ -1,12 +1,13 @@
 import requests
 
 def get_cookie(ticket_url):
-    response = requests.get(ticket_url)
+    response = requests.get(ticket_url, allow_redirects=False)
 
     set_cookie_header = response.headers.get("Set-Cookie", None)
     if set_cookie_header:
-        print("Set-Cookie 中的 Cookie：", set_cookie_header)
         return set_cookie_header
+    else:
+        return None
 
 if __name__ == "__main__":
     get_cookie("http://jw.hitsz.edu.cn/casLogin?ticket=TICKET")
