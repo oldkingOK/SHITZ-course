c98e5b611bcba44e9f53b254cb2b9de01971a7ea
oldkingOK <oldkingok@qq.com>
2024-12-31 13:01:16 +0800

    add ClassType

--- app.py
+++ app.py
@@ -1,32 +1,78 @@
 from get_ticket import get_ticket_url
 from get_cookie import get_cookie
-from grab import grab_class
+from grab import grab_class, ClassType
+from time import sleep
+import os
 
 ticket_url = ""
 cookie = ""
+TICKET_PATH = ".ticket"
+COOKIE_PATH = ".cookies"
 
-def ticket():
+if (os.path.exists(TICKET_PATH)):
+    with open(TICKET_PATH, "r") as f:
+        ticket_url = f.read()
+    print("Loaded Ticket URL:", ticket_url)
+
+if (os.path.exists(COOKIE_PATH)):
+    with open(COOKIE_PATH, "r") as f:
+        cookie = f.read()
+    print("Loaded Cookie:", cookie)
+
+def u_ticket():
     global ticket_url
     ticket_url = get_ticket_url()
     if ticket_url:
         print("Ticket URL:", ticket_url)
+        with open(TICKET_PATH, "w") as f:
+            f.write(ticket_url)
     else:
         print("Failed to get ticket URL")
 
-def cookie():
+def u_cookie():
     global cookie
     while True:
         cookie = get_cookie(ticket_url)
+        with open(COOKIE_PATH, "w") as f:
+            f.write(cookie)
         if cookie:
             print("Cookie:", cookie)
             break
 
 ids = [
-    "2922FE7D81D6929AE0630B18F80A71EA"
+    # ( "248E4D6B93A50548E0630C18F80AD971", ClassType.CROSS), # 污染控制微生物学
+    ( "2580B3A200A64FA1E0630C18F80A1F9A", ClassType.COMPETITION)
 ]
 
 def grab():
-    global cookie
-    for id in ids:
-        print("Grabbing class:", id)
-        grab_class(id, cookie)
+    global cookie, ids
+    grabed = []
+    while True:
+        for i in range(len(ids)):
+            id = ids[i][0]
+            type = ids[i][1]
+            if id in grabed:
+                continue
+
+            print("Grabbing class:", id)
+            # u_cookie()
+            msg = grab_class(id, cookie, type)
+            if "操作成功" in msg['message']:
+                print("Grabbed class:", id)
+                grabed.append(id)
+
+            if "该任务已选择" in msg['message']:
+                print("Class already grabbed:", id)
+                grabed.append(id)
+
+            else:
+                print(msg)
+
+            sleep(2)
+
+if __name__ == "__main__":
+    # grab()
+    # u_ticket()
+    u_ticket()
+    u_cookie()
+    pass
--- grab.py
+++ grab.py
@@ -1,23 +1,60 @@
-DATA = "cxsfmt=0&p_pylx=1&mxpylx=1&p_sfgldjr=0&p_sfredis=0&p_sfsyxkgwc=0&p_xktjz=rwtjzyx&p_chaxunxh=&p_gjz=&p_skjs=&p_xn=2024-2025&p_xq=2&p_xnxq=2024-20252&p_dqxn=2024-2025&p_dqxq=1&p_dqxnxq=2024-20251&p_xkfsdm=bx-b-b&p_xiaoqu=&p_kkyx=&p_kclb=&p_xkxs=&p_dyc=&p_kkxnxq=&p_id={}&p_sfhlctkc=0&p_sfhllrlkc=0&p_kxsj_xqj=&p_kxsj_ksjc=&p_kxsj_jsjc=&p_kcdm_js=&p_kcdm_cxrw=&p_kc_gjz=&p_xzcxtjz_nj=&p_xzcxtjz_yx=&p_xzcxtjz_zy=&p_xzcxtjz_zyfx=&p_xzcxtjz_bj=&p_sfxsgwckb=1&p_skyy=&p_chaxunxkfsdm=&pageNum=1&pageSize=21"
+DATA = "cxsfmt=0&p_pylx=1&mxpylx=1&p_sfgldjr=0&p_sfredis=0&p_sfsyxkgwc=0&p_xktjz=rwtjzyx&p_chaxunxh=&p_gjz=&p_skjs=&p_xn=2024-2025&p_xq=2&p_xnxq=2024-20252&p_dqxn=2024-2025&p_dqxq=1&p_dqxnxq=2024-20251&p_xkfsdm={}&p_xiaoqu=&p_kkyx=&p_kclb=&p_xkxs=&p_dyc=&p_kkxnxq=&p_id={}&p_sfhlctkc=0&p_sfhllrlkc=0&p_kxsj_xqj=&p_kxsj_ksjc=&p_kxsj_jsjc=&p_kcdm_js=&p_kcdm_cxrw=&p_kc_gjz=&p_xzcxtjz_nj=&p_xzcxtjz_yx=&p_xzcxtjz_zy=&p_xzcxtjz_zyfx=&p_xzcxtjz_bj=&p_sfxsgwckb=1&p_skyy=&p_chaxunxkfsdm=&pageNum=1&pageSize=21"
 URL = "http://jw.hitsz.edu.cn/Xsxk/addGouwuche"
 COOKIE = "JSESSIONID=452098A4335A003914041BD920F39EBE; Path=/; HttpOnly, route=2f5ea2943ce43bccdc54bd1e6f73f716; Path=/"
 ID = "2A2CD36B53159179E0630B18F80A81BE"
 
 import requests
 
-def grab_class(id, cookie):
+class ClassType:
+    MUST = "bx-b-b" 
+    """必修课程体系"""
+    LIMIT = "xx-b-b" 
+    """限选课程体系"""
+    CROSS_IN_PLAN = "fankzy-b-b" 
+    """专业内跨专业课程体系"""
+    CROSS = "sx-b-b" 
+    """跨专业课程体系"""
+    COMPETITION = "jsrw-b-b"
+    """竞赛指导课程体系"""
+
+
+def grab_class(id, cookie, type=ClassType.MUST):
     headers = {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Origin": "http://jw.hitsz.edu.cn",
-        "Referer": "http://jw.hitsz.edu.cn/Xsxk/query"
+        "Referer": "http://jw.hitsz.edu.cn/Xsxk/query/1",
+        "Connection": "keep-alive",
+        "X-Requested-With": "XMLHttpRequest",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
     }
 
-    cookies = {i.split("=")[0]: i.split("=")[1] for i in cookie.split(", ")}
+    cookies = {
+        "JSESSIONID": cookie.split(";")[0].split("=")[1],
+        "route": cookie.split(";")[2].split("=")[1]
+    }
+    
+    # cookies = {
+    #     "JSESSIONID": "A879873B12E1D04CD2B0AAE8627A7DB5",
+    #     "route": "1db2c5f6085b9278d9cf7aaa8af65cd2"
+    # }
+
+    print(cookies)
     response = requests.post(URL, 
-                            data=DATA.format(id), 
+                            data=DATA.format(type, id), 
                             cookies=cookies,
-                            headers=headers)
-    print(response.text)
+                            headers=headers,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
+        print("Failed to grab class")
+        return None
+    
+    try:
+        return response.json()
+    except:
+        print("Failed to parse response as JSON")
+        # print(response.text)
+        return response.text
 
 if __name__ == "__main__":
     grab_class(ID, COOKIE)
